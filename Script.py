233b42ab255891f9af123e238160bbe945b3589e
DEVILJO2018 <deviljo2018@gmail.com>
2026-06-28 22:10:08 +0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,23 +1,12 @@
 class script(object):
-       START_TXT = """
-🍿 <b>WELCOME TO KEJO CINEMAS BOT</b> 🍿
+START_TXT = GSTART_TXT = """🍿 <b>WELCOME TO KEJO CINEMAS BOT</b> 🍿
 
 HEY {mention} , GOOD MORNING ☀️
 
 🤖 I AM {bot} , THE MOST POWERFUL AUTO FILTER BOT 🎬
 
 ✨ JOIN OUR MAIN CHANNEL TO GET LATEST MOVIES AND UPDATES 👇
-👉 https://t.me/KejoCinemas1"""
-
-    GSTART_TXT = """
-🍿 <b>WELCOME TO KEJO CINEMAS BOT</b> 🍿
-
-HEY {mention} , GOOD MORNING ☀️
-
-🤖 I AM {bot} , THE MOST POWERFUL AUTO FILTER BOT 🎬
-
-✨ JOIN OUR MAIN CHANNEL TO GET LATEST MOVIES AND UPDATES 👇
-👉 https://t.me/KejoCinemas1"""     
+👉 https://t.me/KejoCinemas1"""      
    
    HELP_TXT = """<b>
     
